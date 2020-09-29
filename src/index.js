e3f194b6624bc30ed893cb22f23d2053744eb968
Nikolai Minkevich <minkevich.nikolai@yandex.ru>
2020-09-29 18:49:41 +0300

    init: start typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length==0) return 0;
+  let min = array[0]
+  array.forEach(element => {
+    if (element<min) min=element
+  });
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length==0) return 0;
+  let max = array[0]
+  array.forEach(element => {
+    if (element>max) max=element
+  });
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array || array.length==0) return 0;
+  let avg = 0
+  let sum = 0
+  array.forEach(element => {
+    sum+=element
+  });
+  avg=sum/array.length
+  return avg;
 }
